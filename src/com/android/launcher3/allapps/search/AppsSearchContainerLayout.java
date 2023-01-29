bananadroid/android_packages_apps_Launcher3

Launcher3: Rework hotseat and all apps search bar
* integrate google logo to "All apps" sb (if available)
* add ability to use material themed icon style for both SBs
* remove AOSP's headercolor protection paint on scroll offset
* sync strings with unified pixel launcher
* misc color and padding adjustments

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -23,6 +23,7 @@ import static com.android.launcher3.Utilities.prefixTextWithIcon;
 import static com.android.launcher3.icons.IconNormalizer.ICON_VISIBLE_AREA_FACTOR;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.graphics.Rect;
 import android.text.Selection;
 import android.text.SpannableStringBuilder;
@@ -36,6 +37,7 @@ import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.ExtendedEditText;
 import com.android.launcher3.Insettable;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.allapps.ActivityAllAppsContainerView;
 import com.android.launcher3.allapps.AllAppsStore;
 import com.android.launcher3.allapps.BaseAllAppsAdapter.AdapterItem;
@@ -115,6 +117,10 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
+        Drawable gIcon = getContext().getDrawable(R.drawable.ic_super_g_color);
+        Drawable gIconThemed = getContext().getDrawable(R.drawable.ic_super_g_themed);
+        Drawable sIcon = getContext().getDrawable(R.drawable.ic_allapps_search);
+        
         // Shift the widget horizontally so that its centered in the parent (b/63428078)
         View parent = (View) getParent();
         int availableWidth = parent.getWidth() - parent.getPaddingLeft() - parent.getPaddingRight();
@@ -122,6 +128,14 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         int expectedLeft = parent.getPaddingLeft() + (availableWidth - myWidth) / 2;
         int shift = expectedLeft - left;
         setTranslationX(shift);
+
+        if (Utilities.showQSB(getContext()) && !Utilities.isThemedIconsEnabled(getContext())) {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(gIcon, null, null, null);
+        } else if (Utilities.showQSB(getContext()) && Utilities.isThemedIconsEnabled(getContext())) {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(gIconThemed, null, null, null);
+        } else {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(sIcon, null, null, null);
+        }
 
         offsetTopAndBottom(mContentOverlap);
     }

lighthouseQC/packages_apps_Launcher3

Signed-off-by: Stealth1226 <rishawn@projectlighthouse.tech>

src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -23,6 +23,7 @@ import static com.android.launcher3.Utilities.prefixTextWithIcon;
 import static com.android.launcher3.icons.IconNormalizer.ICON_VISIBLE_AREA_FACTOR;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.graphics.Rect;
 import android.text.Selection;
 import android.text.SpannableStringBuilder;
@@ -36,12 +37,14 @@ import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.ExtendedEditText;
 import com.android.launcher3.Insettable;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.allapps.ActivityAllAppsContainerView;
 import com.android.launcher3.allapps.AllAppsStore;
 import com.android.launcher3.allapps.BaseAllAppsAdapter.AdapterItem;
 import com.android.launcher3.allapps.SearchUiManager;
 import com.android.launcher3.search.SearchCallback;
 import com.android.launcher3.views.ActivityContext;
+import com.android.launcher3.util.Themes;
 
 import java.util.ArrayList;
 
@@ -115,6 +118,10 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
+        Drawable gIcon = getContext().getDrawable(R.drawable.ic_super_g_color);
+        Drawable gIconThemed = getContext().getDrawable(R.drawable.ic_super_g_themed);
+        Drawable sIcon = getContext().getDrawable(R.drawable.ic_allapps_search);
+        
         // Shift the widget horizontally so that its centered in the parent (b/63428078)
         View parent = (View) getParent();
         int availableWidth = parent.getWidth() - parent.getPaddingLeft() - parent.getPaddingRight();
@@ -122,6 +129,14 @@ public class AppsSearchContainerLayout extends ExtendedEditText
         int expectedLeft = parent.getPaddingLeft() + (availableWidth - myWidth) / 2;
         int shift = expectedLeft - left;
         setTranslationX(shift);
+
+        if (Utilities.showQSB(getContext()) && !Themes.isThemedIconEnabled(getContext())) {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(gIcon, null, null, null);
+        } else if (Utilities.showQSB(getContext()) && Themes.isThemedIconEnabled(getContext())) {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(gIconThemed, null, null, null);
+        } else {
+          setCompoundDrawablesRelativeWithIntrinsicBounds(sIcon, null, null, null);
+        }
 
         offsetTopAndBottom(mContentOverlap);
     }